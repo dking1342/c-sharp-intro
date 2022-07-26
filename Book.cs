30dea73264bd9d00448271f7ac9a822e390d0408
David <leanmachine42@yahoo.com>
2022-07-25 23:18:06 -0500

    made a constructor for the Book class

--- Book.cs
+++ Book.cs
@@ -6,6 +6,14 @@ namespace csharp1
     public string author;
     public int pages;
 
+    // constructor for the class
+    // special method when calling a new object
+    public Book(string titleInput, string authorInput, int pagesInput){
+      title = titleInput;
+      author = authorInput;
+      pages = pagesInput;
+    }
+
     // static void Main(string[] args)
     // {
 
--- Program.cs
+++ Program.cs
@@ -4,11 +4,8 @@
     {
         static void Main(string[] args)
         {  
-          // creating a new book object
-          Book book1 = new Book();
-          book1.title = "lotr";
-          book1.author = "kavooce";
-          book1.pages = 400;
+          // creating a new book object with constructor
+          Book book1 = new Book("lotr","kavooce",400);
 
           Console.WriteLine($"Title: {book1.title}");
           Console.WriteLine($"Author: {book1.author}");
