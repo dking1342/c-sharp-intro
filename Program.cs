c7e45cbc33ad05533b30c608192a871694088293
David <leanmachine42@yahoo.com>
2022-07-25 21:27:30 -0500

    return for methods

--- Program.cs
+++ Program.cs
@@ -4,24 +4,17 @@
     {
         static void Main(string[] args)
         {   
-          Console.Write("Enter your name: ");
-          string user = Console.ReadLine()!;
-          Console.Write("Enter your age: ");
-          int age = Convert.ToInt32(Console.ReadLine());
+          double power1 = cube(2);
+          double power2 = cube(4);
+          double power3 = cube(7);
 
-          greet(user, age); // call the method inside the other method
-          greet("Jack");
+          Console.WriteLine(power1);
+          Console.WriteLine(power2);
+          Console.WriteLine(power3);
         }
 
-        // methods - block of code to perform a task
-        // first keyword denotes the availability of the method (public, private, static, abstract, etc)
-        // second keyword denotes the return value (void, string, int, etc)
-        // third keyword in the name of the method
-        // fourth keyword is the parameter if there is one. the parameter must be data typed
-        // default value can be given to a parameter if required
-        static void greet(string name, int age = 30){
-          Console.WriteLine($"howdy {name}!"); // takes in the parameter within the method
-          Console.WriteLine($"you are {age} years old and you are so young!"); // takes in the parameter within the method
+        static double cube(double num = 1){
+          return Math.Pow(num, 3);
         }
     }
 }
