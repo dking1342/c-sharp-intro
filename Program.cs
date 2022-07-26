7876309386cd8b19b5fe0d2822eb94785a3a4482
David <leanmachine42@yahoo.com>
2022-07-25 20:17:40 -0500

    strings

--- Program.cs
+++ Program.cs
@@ -4,19 +4,28 @@
     {
         static void Main(string[] args)
         {
-          string phrase = "howdy"; // string is a series of characters
-          char letter = 'D'; // single quotation and only one character
-          int score = 7; // whole numbers
-          double temperature = 45.5D; // decimal numbers
-          float gpa = 3.75F; // float number
-          bool isAvailable = true; // true or false 
+          // making string variables
+          string football = "Denver Broncos";
+          string hockey = "Colorado\nAvs";
 
-          Console.WriteLine($"String: {phrase}");
-          Console.WriteLine($"Char: {letter}");
-          Console.WriteLine($"Int: {score}");
-          Console.WriteLine($"Double: {temperature}");
-          Console.WriteLine($"Float: {gpa}");
-          Console.WriteLine($"Boolean: {isAvailable}");
+          Console.WriteLine($"Football: {football}");
+          Console.WriteLine($"Hockey: {hockey}");
+
+          // length of string
+          int footballLength = football.Length;
+          int hockeyLength = hockey.Length;
+
+          Console.WriteLine($"Football length: {footballLength}");
+          Console.WriteLine($"Hockey length: {hockeyLength}");
+
+          // modifying string
+          Console.WriteLine(football.ToUpper()); // capitalizes string
+          Console.WriteLine(football.ToLower()); // lowercase string
+          Console.WriteLine(football.Contains("Denver")); // check if string contains search parameter
+          Console.WriteLine(football.Equals("Detroit Tigers")); // checks for equality
+          Console.WriteLine(football[0]); // find character of string by searching index value
+          Console.WriteLine(football.IndexOf("e")); // finds index position of search parameter
+          
         }
     }
 }
