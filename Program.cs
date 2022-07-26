1dda48928775a3c027b926f8746de0667aaead45
David <leanmachine42@yahoo.com>
2022-07-25 19:40:32 -0500

    writing to the console

--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); // command to write text to the console
+            Console.ReadLine(); // hold any further execution until user reads the line
+
+            // making a triangle in the console
+            Console.WriteLine("  /|");
+            Console.WriteLine(" / |");
+            Console.WriteLine("/__|");
+            Console.ReadLine(); // hold any further execution until user reads the line
         }
     }
 }
