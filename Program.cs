8601d163ac642e3c8ab6cc9d39adba7ce0d8600a
David <leanmachine42@yahoo.com>
2022-07-25 20:04:59 -0500

    data types

--- Program.cs
+++ Program.cs
@@ -4,36 +4,19 @@
     {
         static void Main(string[] args)
         {
-          // creating new variables
-          // you can intentionally type the variable or it can be inferred
-          string name = "Jack";
-          int age = 30;
+          string phrase = "howdy"; // string is a series of characters
+          char letter = 'D'; // single quotation and only one character
+          int score = 7; // whole numbers
+          double temperature = 45.5D; // decimal numbers
+          float gpa = 3.75F; // float number
+          bool isAvailable = true; // true or false 
 
-          // setting variable for later use
-          string friend;
-          friend = "Joe";
-
-          Console.WriteLine($"There once was a man name {name} who had a friend named {friend}");
-          Console.WriteLine($"He was {age} years old");
-          name = "Jill"; // variables can be modified
-          Console.WriteLine($"He really liked the name {name}");
-          Console.WriteLine($"But he didn't like being {age}");
-
-          // declaring variables that do not change value with const
-          const string location = "Japan"; // this variable cannot be overwritten
-          Console.WriteLine($"{name} lives in {location}");
-
-          // declaring variables with different data types
-          int myNum = 5;
-          double myDoubleNum = 5.99D;
-          char myLetter = 'D';
-          bool myBool = true;
-          string myText = "Hello";
-          Console.WriteLine(myNum);
-          Console.WriteLine(myDoubleNum);
-          Console.WriteLine(myLetter);
-          Console.WriteLine(myBool);
-          Console.WriteLine(myText);
+          Console.WriteLine($"String: {phrase}");
+          Console.WriteLine($"Char: {letter}");
+          Console.WriteLine($"Int: {score}");
+          Console.WriteLine($"Double: {temperature}");
+          Console.WriteLine($"Float: {gpa}");
+          Console.WriteLine($"Boolean: {isAvailable}");
         }
     }
 }
