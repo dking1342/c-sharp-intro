ea9316bd07803f2b68edc3626713e418ed7dcdff
David <leanmachine42@yahoo.com>
2022-07-25 20:48:13 -0500

    arrays

--- Program.cs
+++ Program.cs
@@ -4,14 +4,34 @@
     {
         static void Main(string[] args)
         {   
-          // storing user inputs into variables then display it later
-          Console.Write("Enter your name: ");
-          string name = Console.ReadLine()!;
-          Console.Write("Enter your age: ");
-          int age = Convert.ToInt32(Console.ReadLine());
+          // arrays
+          // creating array variables for different data types
+          string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+          Console.WriteLine(cars[0]);
+          cars[0] = "Buick";
+          Console.WriteLine(cars[0]);
 
-          Console.WriteLine($"Username: {name}");
-          Console.WriteLine($"Age: {age}");
+          int[] ages = {10, 200, 30, 40};
+          Console.WriteLine($"Ages length: {ages.Length}");
+          Array.Sort(ages);
+
+          Console.WriteLine($"Max age: {ages.Max()}");  // returns the largest value
+          Console.WriteLine($"Min age: {ages.Min()}");  // returns the smallest value
+          Console.WriteLine($"Age sum: {ages.Sum()}");  // returns the sum of elements
+
+          // other ways to create an array
+          // Create an array of four elements, and add values later
+          string[] vehicles1 = new string[4];
+
+          // Create an array of four elements and add values right away 
+          string[] vehicles2 = new string[4] {"Volvo", "BMW", "Ford", "Mazda"};
+
+          // Create an array of four elements without specifying the size 
+          string[] vehicles3 = new string[] {"Volvo", "BMW", "Ford", "Mazda"};
+
+          // Create an array of four elements, omitting the new keyword, and without specifying the size
+          string[] vehicles4 = {"Volvo", "BMW", "Ford", "Mazda"};
+
         }
     }
 }
