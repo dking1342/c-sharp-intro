a881311012113febf1d93690bb0ebed1ffa89233
David <leanmachine42@yahoo.com>
2022-07-25 22:35:17 -0500

    switch statements

--- Program.cs
+++ Program.cs
@@ -4,96 +4,64 @@
     {
         static void Main(string[] args)
         {  
-          // structure of an if statement
-          if (4 > 5) 
-          {
-            // block of code to be executed if the condition is True
-          }
-
-          // testing numbers
-          if (20 > 18) 
-          {
-            Console.WriteLine("20 is greater than 18");
-          }
-
-          // testing variables
-          int x = 20;
-          int y = 18;
-          if (x > y) 
-          {
-            Console.WriteLine("x is greater than y");
-          }
-
-          // if/else structure
-          if (5 == 5)
-          {
-            // block of code to be executed if the condition is True
-          } 
-          else 
-          {
-            // block of code to be executed if the condition is False
-          }
-
-          // example with time of day
-          int time = 20;
-          if (time < 18) 
-          {
-            Console.WriteLine("Good day.");
-          } 
-          else 
-          {
-            Console.WriteLine("Good evening.");
-          }
-          // Outputs "Good evening."
-
-          // if/else if/else structure
-          // if (condition)
+          // switch structure
+          // switch(expression) 
           // {
-          //   // block of code to be executed if condition1 is True
-          // } 
-          // else if (condition) 
-          // {
-          //   // block of code to be executed if the condition1 is false and condition2 is True
-          // } 
-          // else
-          // {
-          //   // block of code to be executed if the condition1 is false and condition2 is False
+          //   case x:
+          //     // code block
+          //     break;
+          //   case y:
+          //     // code block
+          //     break;
+          //   default:
+          //     // code block
+          //     break;
           // }
 
-          // example with if/else if/else
-          int hour = 22;
-          if (hour < 10) 
+          int day = 4;
+          switch (day) 
           {
-            Console.WriteLine("Good morning.");
-          } 
-          else if (hour < 20) 
-          {
-            Console.WriteLine("Good day.");
-          } 
-          else 
-          {
-            Console.WriteLine("Good evening.");
+            case 1:
+              Console.WriteLine("Monday");
+              break;
+            case 2:
+              Console.WriteLine("Tuesday");
+              break;
+            case 3:
+              Console.WriteLine("Wednesday");
+              break;
+            case 4:
+              Console.WriteLine("Thursday");
+              break;
+            case 5:
+              Console.WriteLine("Friday");
+              break;
+            case 6:
+              Console.WriteLine("Saturday");
+              break;
+            case 7:
+              Console.WriteLine("Sunday");
+              break;
           }
-          // Outputs "Good evening."
+          // Outputs "Thursday" (day 4)
 
-          // short hand structure -- ternary operator
-          // variable = (condition) ? expressionTrue :  expressionFalse;
-          // example of ternary
-          int timeOfDay = 20;
-          string result = (timeOfDay < 18) ? "Good day." : "Good evening.";
-          Console.WriteLine(result);
 
-          // logical operators can be included as well
-          bool isMale = true;
-          bool isTall = false;
-
-          if( isMale && isTall){
-            Console.WriteLine("you are male and you are tall");
-          } else if( isMale || isTall){
-            Console.WriteLine("you are either male or tall");
-          } else {
-            Console.WriteLine("you are neither male nor tall");
+          // example using default
+          int today = 4;
+          switch (today) 
+          {
+            case 6:
+              Console.WriteLine("Today is Saturday.");
+              break;
+            case 7:
+              Console.WriteLine("Today is Sunday.");
+              break;
+            default:
+              Console.WriteLine("Looking forward to the Weekend.");
+              break;
           }
+          // Outputs "Looking forward to the Weekend."
+
         }
 
     }
