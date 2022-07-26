2bba5c55623fd3298e1f4dc27d03506c0c53efe1
David <leanmachine42@yahoo.com>
2022-07-25 19:55:18 -0500

    vars

--- Program.cs
+++ Program.cs
@@ -4,14 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); // command to write text to the console
-            Console.ReadLine(); // hold any further execution until user reads the line
+          // creating new variables
+          // you can intentionally type the variable or it can be inferred
+          string name = "Jack";
+          int age = 30;
 
-            // making a triangle in the console
-            Console.WriteLine("  /|");
-            Console.WriteLine(" / |");
-            Console.WriteLine("/__|");
-            Console.ReadLine(); // hold any further execution until user reads the line
+          // setting variable for later use
+          string friend;
+          friend = "Joe";
+
+          Console.WriteLine($"There once was a man name {name} who had a friend named {friend}");
+          Console.WriteLine($"He was {age} years old");
+          name = "Jill"; // variables can be modified
+          Console.WriteLine($"He really liked the name {name}");
+          Console.WriteLine($"But he didn't like being {age}");
+
+          // declaring variables that do not change value with const
+          const string location = "Japan"; // this variable cannot be overwritten
+          Console.WriteLine($"{name} lives in {location}");
+
+          // declaring variables with different data types
+          int myNum = 5;
+          double myDoubleNum = 5.99D;
+          char myLetter = 'D';
+          bool myBool = true;
+          string myText = "Hello";
+          Console.WriteLine(myNum);
+          Console.WriteLine(myDoubleNum);
+          Console.WriteLine(myLetter);
+          Console.WriteLine(myBool);
+          Console.WriteLine(myText);
         }
     }
 }
